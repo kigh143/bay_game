f2c269344653633573bc6fa528564c52c8a00f0c
kigh143 <ghkatende@gmail.com>
2018-08-22 11:36:45 +0300

    added boostrap

---
+++ src/app/components/Scoreboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export class Scoreboard extends React.Component {
+    render (){
+        return (
+            <div>
+                <h5>Score Board</h5>
+                <hr/>
+                <p>steps taken : 696</p>
+                <p>Time taken : 6:50:20</p>
+            </div>
+        );
+    }
+}
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,23 @@
-console.log("react app");
+import React from "react";
+import { render } from "react-dom";
+import {  Scoreboard  } from "./components/Scoreboard";
+import {  Playground  } from "./components/Playground";
+
+class App extends React.Component {
+    render (){
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-8">
+                        <Playground/>
+                    </div>
+                    <div className="col-md-4">
+                        <Scoreboard/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+render ( <App/>, window.document.getElementById("app")); 
