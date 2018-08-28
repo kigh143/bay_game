de11f7bdca5455c0c7a6b55e41fa35654b6a9483
kigh143 <ghkatende@gmail.com>
2018-08-29 00:52:57 +0300

    adding movePlayer, create_player and create_sprite function

--- src/app/components/Playground.js
+++ src/app/components/Playground.js
@@ -6,9 +6,68 @@ export class Playground extends React.Component {
         super();
         this.state = {
             boradSize: 0,
+            cell_size:40,
+            size:1000
         }
     }
-    createBoard = () => {
+
+    createBoard = (event) => {
+
+        this.setState({ boradSize: parseInt(event.target.value) });
+
+        var cols = this.state.boradSize;
+        var rows = this.state.boradSize;
+        var c_canvas = this.refs.canvas;
+
+        window.addEventListener("keypress", this.movePlayer);
+
+        var ctx = c_canvas.getContext("2d");
+
+        let size = this.state.cell_size * cols;
+        ctx.clearRect(0,0,size,size);
+        for( let i=0; i<rows; i++){
+            for( let j=0; j<cols; j++){
+                var x =i*this.state.cell_size;
+                var y =j*this.state.cell_size;
+                ctx.rect(x,y,this.state.cell_size,this.state.cell_size);
+            }
+        }
+        ctx.stroke();
+        this.create_play(ctx);
+        this.create_sprite(ctx);
+    }
+
+
+    create_sprite = (context) => {
+        let sprite_total = parseInt(this.state.boradSize);
+        for( let i = 0; i < sprite_total; i++ ){
+            let yPosition = Math.floor(Math.random() * sprite_total)*this.state.cell_size;
+            let xPosition = Math.floor(Math.random() * sprite_total)*this.state.cell_size;
+            context.fillStyle="green";
+            context.fillRect(xPosition, yPosition, this.state.cell_size, this.state.cell_size);
+        }
+    }
+
+    create_play = (context) => {
+        let sprite_total = parseInt(this.state.boradSize);
+        let yPosition = Math.floor(Math.random() * sprite_total)*this.state.cell_size;
+        let xPosition = Math.floor(Math.random() * sprite_total)*this.state.cell_size;
+        context.fillStyle="orange";
+        context.fillRect(xPosition, yPosition, this.state.cell_size, this.state.cell_size);
+    }
+
+
+    movePlayer = (event) => {
+        console.log( event ); 
+        if(event.keyCode == 100){
+
+        } else if(event.keyCode == 1){
+
+        } else if(event.keyCode == 100){
+
+        } else if(event.keyCode == 100){
+
+        }
 
     }
 
@@ -21,7 +80,7 @@ export class Playground extends React.Component {
                         <div className="input-group-prepend">
                         <label class="input-group-text" for="inputGroupSelect01">Game levels</label>
                         </div>
-                        <select className="custom-select" id="inputGroupSelect01" onChange={(event) => this.changevalue(event)}>
+                        <select className="custom-select" id="inputGroupSelect01" onChange={(event) => this.createBoard(event)}>
                         <option selected>Choose level</option>
                         <option value="8">8</option>
                         <option value="10">10</option>
@@ -32,9 +91,6 @@ export class Playground extends React.Component {
                         <button onClick={this.createBoard } className="btn btn-primary"> start game</button>
                     </div>
                 </div>
-                <span>Game charaters : </span>
-                <img ref="img"  src={"../../imgs/rat.png"}  className="img"/>
-                <img ref="img2"  src={"../../imgs/cat.png"}  className="img"/>
                 <div className="canvas">
                     <canvas ref="canvas" width={this.state.size} height={this.state.size}></canvas>
                 </div>
